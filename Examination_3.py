61bd9abed8d400f216c9bfaecb614183c0e875a1
numphet <27979@ud.ac.th>
2026-09-16 10:48:46 +0700

    Update Examination_3.py

--- Examination_3.py
+++ Examination_3.py
@@ -5,5 +5,8 @@
 # ==========================================
 
 
-# นักเรียนเขียนโค้ดต่อจากบรรทัดนี้
-score = int(input())
+score = int(input("คะแนนสอบ: "))
+if score>=50
+print("Pass")
+elif score<=49
+print("Fail")
